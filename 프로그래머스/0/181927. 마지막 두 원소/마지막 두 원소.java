4fcf84b6d97554440943cf9db17923af5bcd6520
t189216 <tus0926@naver.com>
2025-05-02 22:20:02 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 class Solution {
     public List solution(int[] num_list) {
-        List<Integer> answer = new ArrayList<>() ;
+        List<Integer> answer = new ArrayList<>();
         int tmp = 0;
+        
+        int last = num_list[num_list.length - 1];
+        int lastb = num_list[num_list.length - 2];
         
         for (int i = 0; i < num_list.length; i++) {
             answer.add(num_list[i]);
         }       
         
-        tmp = num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length - 1] - num_list[num_list.length - 2] : (num_list[num_list.length - 1]) * 2;
+        tmp = last > lastb ? last - lastb : (last) * 2;
         
         answer.add(tmp);
             
